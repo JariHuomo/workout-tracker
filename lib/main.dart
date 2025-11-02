06426edc20b6e6024b06e7c4a778cc1098029317
JariHuomo <jari.huomo@gmail.com>
2025-11-02 17:27:13 +0200

    Auto-commit from ElectronAITool

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:workouttracker/src/routing/app_router.dart';
@@ -5,11 +7,31 @@ import 'package:workouttracker/src/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: MyApp()));
+  final enableDevicePreview = _shouldEnableDevicePreview();
+  final app = ProviderScope(
+    child: MyApp(devicePreviewEnabled: enableDevicePreview),
+  );
+
+  if (enableDevicePreview) {
+    runApp(DevicePreview(builder: (context) => app));
+    return;
+  }
+
+  runApp(app);
+}
+
+bool _shouldEnableDevicePreview() {
+  if (kReleaseMode || kIsWeb) {
+    return false;
+  }
+
+  return defaultTargetPlatform == TargetPlatform.windows;
 }
 
 class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, this.devicePreviewEnabled = false});
+
+  final bool devicePreviewEnabled;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,6 +43,9 @@ class MyApp extends ConsumerWidget {
       theme: theme.light,
       darkTheme: theme.dark,
       themeMode: theme.mode,
+      builder: devicePreviewEnabled ? DevicePreview.appBuilder : null,
+      locale: devicePreviewEnabled ? DevicePreview.locale(context) : null,
+      useInheritedMediaQuery: devicePreviewEnabled,
       routerConfig: router,
     );
   }
